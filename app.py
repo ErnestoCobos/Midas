857861ffe7dee1ebe94d478b1956d8cd9ef0c25b
Ernesto Cobos <ernesto@cobos.io>
2024-03-23 18:56:40 +0000

    feat: cdk infrastructure to deploy midas workers

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import os
 
-import aws_cdk as cdk
+from aws_cdk import App
 
 
 from midas.midas_stack import MidasStack
 
 
-app = cdk.core.App()
+app = App()
 MidasStack(app, "MidasStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
